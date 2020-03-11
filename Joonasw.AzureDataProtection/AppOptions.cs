0880cb95d08c220ce7fb8d754192da7f0ebe24bf
Joonas Westlin <joonas.westlin@zure.com>
2020-03-11 10:59:25 +0200

    Initial commit

---
+++ Joonasw.AzureDataProtection/AppOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Joonasw.AzureDataProtection
+{
+    public class AppOptions
+    {
+        public string KeyVaultKeyId { get; set; }
+        public string StorageAccountBlobBaseUrl { get; set; }
+        public string StorageContainerName { get; set; }
+        public string StorageBlobName { get; set; }
+        public string SharedTokenCacheTenantId { get; set; }
+    }
+}
---
+++ Joonasw.AzureDataProtection/Startup.cs
@@ -0,0 +1,99 @@
+using Azure.Core;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Joonasw.AzureDataProtection
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
+                .AddAzureAD(options => _configuration.Bind("AzureAd", options));
+
+            services.AddRazorPages().AddMvcOptions(options =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            var options = _configuration.GetSection("App").Get<AppOptions>();
+
+            var keyIdentifier = options.KeyVaultKeyId;
+            services.AddDataProtection()
+                .ProtectKeysWithAzureKeyVault(
+                    keyIdentifier,
+                    GetTokenCredential(options))
+                .PersistKeysToAzureBlobStorage(GetBlobClient(options));
+        }
+
+        private BlobClient GetBlobClient(AppOptions options)
+        {
+            var client = new BlobServiceClient(
+                new Uri(options.StorageAccountBlobBaseUrl),
+                GetTokenCredential(options));
+
+            var containerName = options.StorageContainerName;
+            BlobContainerClient containerClient = client.GetBlobContainerClient(containerName);
+            var blobName = options.StorageBlobName;
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+            return blobClient;
+        }
+
+        private TokenCredential GetTokenCredential(AppOptions options)
+        {
+            return new DefaultAzureCredential(new DefaultAzureCredentialOptions
+            {
+                SharedTokenCacheTenantId = options.SharedTokenCacheTenantId
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
